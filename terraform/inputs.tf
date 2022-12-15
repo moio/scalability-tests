9960070194c726a209ede15fd4800f254282ea2a
Silvio Moioli <silvio@moioli.net>
2022-12-15 16:19:41 +0100

    Adapt main.tf
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -1,11 +1,35 @@
+variable "upstream_datastore" {
+  type        = string
+  description = "Use 'etcd' to deploy a dedicated etcd cluster, 'postgres' for a dedicated Postgres DB, null for embedded etcd"
+}
+
+variable "kine_executable" {
+  type        = string
+  description = "Path to an alternative kine executable or do not set for current kine version"
+  default     = null
+}
+
 locals {
+  region                      = "us-east-1"
+  availability_zone           = "us-east-1a"
+  secondary_availability_zone = "us-east-1b"
+
+  bastion_ami = "ami-08ee40d4e354946c6" // amazon/suse-sles-15-sp4-v20221216-hvm-ssd-arm64
+
+  postgres_ami           = "ami-0b5eea76982371e91" // Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  postgres_instance_type = "m6id.4xlarge"
+
+  etcd_ami           = "ami-0b5eea76982371e91" // Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  etcd_instance_type = "m6id.4xlarge"
+  etcd_server_count  = 3
+
+  upstream_instance_type  = "t3a.xlarge"
+  upstream_ami            = "ami-0ea1c7db66fee3098" // Ubuntu: us-east-1 jammy 22.04 LTS amd64 hvm:ebs-ssd 20230106
   upstream_server_count   = 3
   upstream_agent_count    = 0
   upstream_distro_version = "v1.24.6+k3s1"
-  rancher_chart           = "https://releases.rancher.com/server-charts/latest/rancher-2.6.9.tgz"
-  upstream_san            = "upstream.local.gd"
-  upstream_datastore      = "postgres"
-  upstream_enable_pprof   = true
 
-  project_name = "moio"
+  project_name         = "moio"
+  ssh_private_key_path = "~/.ssh/id_ed25519"
+  ssh_public_key_path  = "~/.ssh/id_ed25519.pub"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,124 @@
 terraform {
-  required_version = "1.3.1"
+  required_version = "1.3.7"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.31.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.3"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.7.1"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.23.1"
-    }
-    k3d = {
-      source  = "pvotal-tech/k3d"
-      version = "0.0.6"
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "2.2.1"
     }
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+provider "aws" {
+  region = local.region
 }
 
-module "network" {
-  source       = "./k3d_network"
-  project_name = local.project_name
+module "aws_shared" {
+  source              = "./aws_shared"
+  project_name        = local.project_name
+  ssh_public_key_path = local.ssh_public_key_path
+}
+
+module "aws_network" {
+  source                      = "./aws_network"
+  region                      = local.region
+  availability_zone           = local.availability_zone
+  secondary_availability_zone = local.secondary_availability_zone
+  project_name                = local.project_name
+}
+
+module "bastion" {
+  depends_on            = [module.aws_network]
+  source                = "./aws_host"
+  ami                   = local.bastion_ami
+  availability_zone     = local.availability_zone
+  project_name          = local.project_name
+  name                  = "bastion"
+  ssh_key_name          = module.aws_shared.key_name
+  ssh_private_key_path  = local.ssh_private_key_path
+  subnet_id             = module.aws_network.public_subnet_id
+  vpc_security_group_id = module.aws_network.public_security_group_id
+}
+
+module "etcd" {
+  count                 = var.upstream_datastore == "etcd" ? 1 : 0
+  depends_on            = [module.aws_network]
+  source                = "./aws_etcd"
+  ami                   = local.etcd_ami
+  instance_type         = local.etcd_instance_type
+  availability_zone     = local.availability_zone
+  project_name          = local.project_name
+  name                  = "etcd"
+  server_count          = local.etcd_server_count
+  ssh_key_name          = module.aws_shared.key_name
+  ssh_private_key_path  = local.ssh_private_key_path
+  ssh_bastion_host      = module.bastion.public_name
+  subnet_id             = module.aws_network.private_subnet_id
+  vpc_security_group_id = module.aws_network.public_security_group_id
+}
+
+module "postgres_kine" {
+  count                 = var.upstream_datastore == "postgres" ? 1 : 0
+  depends_on            = [module.aws_network]
+  source                = "./aws_postgres_kine"
+  ami                   = local.postgres_ami
+  instance_type         = local.postgres_instance_type
+  availability_zone     = local.availability_zone
+  project_name          = local.project_name
+  name                  = "postgres"
+  ssh_key_name          = module.aws_shared.key_name
+  ssh_private_key_path  = local.ssh_private_key_path
+  ssh_bastion_host      = module.bastion.public_name
+  subnet_id             = module.aws_network.private_subnet_id
+  vpc_security_group_id = module.aws_network.public_security_group_id
+  kine_executable       = var.kine_executable
 }
 
 module "upstream_cluster" {
-  source                   = "./k3d_k3s"
-  project_name             = local.project_name
-  name                     = "upstream"
-  network_name             = module.network.name
-  server_count             = local.upstream_server_count
-  agent_count              = local.upstream_agent_count
-  distro_version           = local.upstream_distro_version
-  sans                     = [local.upstream_san]
-  datastore                = local.upstream_datastore
-  enable_pprof             = local.upstream_enable_pprof
-  additional_port_mappings = [[3000, 443], [6443, 6443]]
+  depends_on             = [module.etcd, module.postgres_kine]
+  source                 = "./aws_k3s"
+  ami                    = local.upstream_ami
+  instance_type          = local.upstream_instance_type
+  availability_zone      = local.availability_zone
+  project_name           = local.project_name
+  name                   = "upstream"
+  server_count           = local.upstream_server_count
+  agent_count            = local.upstream_agent_count
+  ssh_key_name           = module.aws_shared.key_name
+  ssh_private_key_path   = local.ssh_private_key_path
+  ssh_bastion_host       = module.bastion.public_name
+  subnet_id              = module.aws_network.private_subnet_id
+  vpc_security_group_id  = module.aws_network.private_security_group_id
+  additional_ssh_tunnels = [[3000, 443]]
+  distro_version         = local.upstream_distro_version
+  secondary_subnet_id    = module.aws_network.secondary_private_subnet_id
+  datastore_endpoint = (
+    var.upstream_datastore == "etcd" ? join(",", formatlist("http://%s:2379", module.etcd[0].server_names)) :
+    var.upstream_datastore == "postgres" ? "http://${module.postgres_kine[0].private_name}:2379" :
+    null
+  )
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-module "rancher" {
-  depends_on   = [module.upstream_cluster]
-  count        = local.upstream_server_count > 0 ? 1 : 0
-  source       = "./rancher"
-  public_name  = local.upstream_san
-  private_name = module.upstream_cluster.first_server_private_name
-  chart        = local.rancher_chart
+module "k6" {
+  depends_on            = [module.upstream_cluster]
+  source                = "./aws_k6"
+  availability_zone     = local.availability_zone
+  project_name          = local.project_name
+  name                  = "k6"
+  ssh_key_name          = module.aws_shared.key_name
+  ssh_private_key_path  = local.ssh_private_key_path
+  ssh_bastion_host      = module.bastion.public_name
+  subnet_id             = module.aws_network.private_subnet_id
+  vpc_security_group_id = module.aws_network.public_security_group_id
 }
