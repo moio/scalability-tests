71954e1ff26ac8f7e7f46b7cbf378a3a1f2d2911
Silvio Moioli <silvio@moioli.net>
2023-07-19 17:27:54 +0200

    temp flavio
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/main/aws/inputs.tf
+++ terraform/main/aws/inputs.tf
@@ -1,63 +1,7 @@
 locals {
-  project_name = "moio"
-
-  upstream_cluster = {
-    name           = "upstream"
-    server_count   = 3
-    agent_count    = 2
-    distro_version = "v1.24.12+k3s1"
-    agent_labels   = [
-      [{ key : "monitoring", value : "true" }]
-    ]
-    agent_taints = [
-      [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
-    ]
-
-    // aws-specific
-    local_name    = "upstream.local.gd"
-    instance_type = "i3.large"
-    ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
-  }
-
-  downstream_clusters = [
-  for i in range(5) :
-  {
-    name           = "downstream-${i}"
-    server_count   = 3
-    agent_count    = 7
-    distro_version = "v1.24.12+k3s1"
-    agent_labels   = []
-    agent_taints   = []
-
-    // aws-specific
-    local_name    = "downstream-${i}.local.gd"
-    instance_type = "t4g.large"
-    ami           = "ami-0e55a8b472a265e3f" // openSUSE-Leap-15-5-v20230608-hvm-ssd-arm64
-  }
-  ]
-
-  tester_cluster = {
-    name           = "tester"
-    server_count   = 1
-    agent_count    = 0
-    distro_version = "v1.24.12+k3s1"
-    agent_labels   = []
-    agent_taints   = []
-
-    // aws-specific
-    local_name    = "tester.local.gd"
-    instance_type = "t3a.large"
-    ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
-  }
-
-  clusters = concat([local.upstream_cluster], local.downstream_clusters, [local.tester_cluster])
-
-  // aws-specific
-  first_local_kubernetes_api_port = 7445
-  first_local_http_port           = 9080
-  first_local_https_port          = 9443
-  region                          = "us-east-1"
-  availability_zone               = "us-east-1a"
-  ssh_private_key_path            = "~/.ssh/id_ed25519"   // generate with `ssh-keygen -t ed25519`
-  ssh_public_key_path             = "~/.ssh/id_ed25519.pub"
+  project_name         = "moio"
+  region               = "us-east-1"
+  availability_zone    = "us-east-1a"
+  ssh_private_key_path = "~/.ssh/id_ed25519"   // generate with `ssh-keygen -t ed25519`
+  ssh_public_key_path  = "~/.ssh/id_ed25519.pub"
 }
--- terraform/main/aws/main.tf
+++ terraform/main/aws/main.tf
@@ -36,28 +36,3 @@ module "network" {
   ssh_public_key_path  = local.ssh_public_key_path
   ssh_private_key_path = local.ssh_private_key_path
 }
-
-module "cluster" {
-  count          = length(local.clusters)
-  source         = "../../modules/aws_k3s"
-  project_name   = local.project_name
-  name           = local.clusters[count.index].name
-  server_count   = local.clusters[count.index].server_count
-  agent_count    = local.clusters[count.index].agent_count
-  agent_labels   = local.clusters[count.index].agent_labels
-  agent_taints   = local.clusters[count.index].agent_taints
-  distro_version = local.clusters[count.index].distro_version
-
-  sans                      = [local.clusters[count.index].local_name]
-  local_kubernetes_api_port = local.first_local_kubernetes_api_port + count.index
-  local_http_port           = local.first_local_http_port + count.index
-  local_https_port          = local.first_local_https_port + count.index
-  ami                       = local.clusters[count.index].ami
-  instance_type             = local.clusters[count.index].instance_type
-  availability_zone         = local.availability_zone
-  ssh_key_name              = module.network.key_name
-  ssh_private_key_path      = local.ssh_private_key_path
-  ssh_bastion_host          = module.network.bastion_public_name
-  subnet_id                 = module.network.private_subnet_id
-  vpc_security_group_id     = module.network.private_security_group_id
-}
