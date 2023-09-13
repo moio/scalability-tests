a9ddfde44ef16829bb0aff0c9a9ff7a8cfac7f47
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2023-09-13 21:14:22 +0000

    update tf modules to improve their flexibility

--- terraform/main/aws/inputs.tf
+++ terraform/main/aws/inputs.tf
@@ -1,7 +1,70 @@
 locals {
-  project_name = "st"
+  project_name = var.project_name
 
-  upstream_cluster = {
+  upstream_cluster = var.upstream_cluster
+
+  downstream_clusters = var.num_downstreams > 0 ? [
+    for i in range(var.num_downstreams) :
+    merge(var.downstream_config, {
+      name       = "downstream-${i}"
+      local_name = "downstream-${i}.local.gd"
+      }
+    )
+  ] : []
+
+  tester_cluster = var.tester_cluster
+
+  clusters = concat([local.upstream_cluster], local.downstream_clusters, [local.tester_cluster])
+
+  // aws-specific
+  first_local_kubernetes_api_port = 7445
+  first_local_http_port           = 9080
+  first_local_https_port          = 9443
+  region                          = var.region
+  availability_zone               = var.availability_zone
+  ssh_private_key_path            = var.ssh_private_key_path // generate with `ssh-keygen -t ed25519`
+  ssh_public_key_path             = var.ssh_public_key_path
+}
+
+variable "project_name" {
+  type    = string
+  default = "moio"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "us-east-2a"
+}
+
+variable "ssh_private_key_path" {
+  type = string
+}
+
+variable "ssh_public_key_path" {
+  type = string
+}
+
+variable "upstream_cluster" {
+  type = object({
+    name           = string
+    server_count   = number
+    agent_count    = number
+    distro_version = string
+    agent_labels   = optional(list(list(map(string))))
+    agent_taints   = optional(list(list(map(string))))
+
+    // aws-specific
+    local_name    = string
+    datastore     = optional(string)
+    instance_type = string
+    ami           = string
+  })
+  default = {
     name           = "upstream"
     server_count   = 3
     agent_count    = 2
@@ -18,25 +81,23 @@ locals {
     instance_type = "i3.large"
     ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
   }
+}
 
-  downstream_clusters = [
-    for i in range(5) :
-    {
-      name           = "downstream-${i}"
-      server_count   = 3
-      agent_count    = 7
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = []
-      agent_taints   = []
+variable "tester_cluster" {
+  type = object({
+    name           = string
+    server_count   = number
+    agent_count    = number
+    distro_version = string
+    agent_labels   = optional(list(list(map(string))), [])
+    agent_taints   = optional(list(list(map(string))), [])
 
-      // aws-specific
-      local_name    = "downstream-${i}.local.gd"
-      instance_type = "t4g.large"
-      ami           = "ami-0e55a8b472a265e3f" // openSUSE-Leap-15-5-v20230608-hvm-ssd-arm64
-    }
-  ]
-
-  tester_cluster = {
+    // aws-specific
+    local_name    = string
+    instance_type = string
+    ami           = string
+  })
+  default = {
     name           = "tester"
     server_count   = 1
     agent_count    = 0
@@ -49,24 +110,34 @@ locals {
     instance_type = "t3a.large"
     ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
   }
-
-  clusters = concat([local.upstream_cluster], local.downstream_clusters, [local.tester_cluster])
-
-  // aws-specific
-  first_local_kubernetes_api_port = 7445
-  first_local_http_port           = 9080
-  first_local_https_port          = 9443
-  region                          = "us-east-1"
-  availability_zone               = "us-east-1a"
 }
 
-
-variable "ssh_public_key_path" {
-  description = "Path to SSH public key file (can be generated with `ssh-keygen -t ed25519`)"
-  default     = "~/.ssh/id_ed25519.pub"
+variable "num_downstreams" {
+  type    = number
+  default = 0
 }
 
-variable "ssh_private_key_path" {
-  description = "Path to SSH private key file (can be generated with `ssh-keygen -t ed25519`)"
-  default     = "~/.ssh/id_ed25519"
+variable "downstream_config" {
+  type = object({
+    name           = optional(string)
+    server_count   = number
+    agent_count    = number
+    distro_version = string
+    agent_labels   = optional(list(list(map(string))), [])
+    agent_taints   = optional(list(list(map(string))), [])
+
+    // aws-specific
+    local_name    = optional(string)
+    instance_type = string
+    ami           = string
+  })
+  default = {
+    server_count   = 3
+    agent_count    = 7
+    distro_version = "v1.24.12+k3s1"
+
+    // aws-specific
+    instance_type = "t4g.large"
+    ami           = "ami-0e55a8b472a265e3f" // openSUSE-Leap-15-5-v20230608-hvm-ssd-arm64
+  }
 }
--- terraform/main/aws/main.tf
+++ terraform/main/aws/main.tf
@@ -1,37 +1,36 @@
 terraform {
-  required_version = "1.5.6"
+  required_version = ">= 1.3.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.31.0"
+      version = ">= 4.63.0, <= 5.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "4.0.3"
+      version = ">= 4.0.3, <= 5.0.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.7.1"
+      version = ">= 2.7.1, <= 3.0.0"
     }
     ssh = {
       source  = "loafoe/ssh"
-      version = "2.2.1"
-    }
-    k3d = {
-      source  = "pvotal-tech/k3d"
-      version = "0.0.6"
+      version = ">= 2.2.1, <= 3.0.0"
     }
   }
 }
 
 provider "aws" {
-  region = local.region
+  region  = local.region
+  profile = "rancher-eng"
 }
 
 module "network" {
   source               = "../../modules/aws_network"
   project_name         = local.project_name
   region               = local.region
+  ami                  = local.downstream_clusters[0].ami
+  instance_type        = local.downstream_clusters[0].instance_type
   availability_zone    = local.availability_zone
   ssh_public_key_path  = var.ssh_public_key_path
   ssh_private_key_path = var.ssh_private_key_path
@@ -42,6 +41,7 @@ module "cluster" {
   source         = "../../modules/aws_k3s"
   project_name   = local.project_name
   name           = local.clusters[count.index].name
+  datastore      = try(local.clusters[count.index].name == "upstream" ? local.clusters[count.index].datastore : null, null)
   server_count   = local.clusters[count.index].server_count
   agent_count    = local.clusters[count.index].agent_count
   agent_labels   = local.clusters[count.index].agent_labels
@@ -60,4 +60,5 @@ module "cluster" {
   ssh_bastion_host          = module.network.bastion_public_name
   subnet_id                 = module.network.private_subnet_id
   vpc_security_group_id     = module.network.private_security_group_id
+  depends_on                = [module.network]
 }
